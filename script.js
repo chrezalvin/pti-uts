1aa9aa23b285108a742aa263d6fe8d58bcf2778e
chrezalvin <67034165+chrezalvin@users.noreply.github.com>
2022-10-07 15:50:43 +0700

    first page js full
    js full on first login page, only js so the graphic might be crappy

--- script.js
+++ script.js
@@ -1,4 +1,37 @@
 // shorthand for doc ready
 $(function(){
-    
+
+    const inputPlayer = $("#playerName");
+    const addPlayerBtn = $("#addPlayerBtn")
+    const playGameBtn = $("#playGameBtn")
+
+    const players = [];
+
+    // updates everything, used on all event when it's done
+    function bringUpdate(){
+        const playerListRender = players.map((player) => `<li>${player}</li>`)
+        $("#playerList").html(`
+        <h3>Player List:</h3>
+        ${playerListRender.join('\n')}
+        `);
+    }
+
+    addPlayerBtn.on("click", () => {
+        if(inputPlayer.val() === "") return;
+        if(players.find((name) => name === inputPlayer.val()))
+        {
+            alert("terdapat nama peserta yang sama, mohon diganti dengan nama lain");
+            return;
+        }
+
+        players.push(inputPlayer.val());
+        inputPlayer.val("");
+        inputPlayer.trigger("focus");
+        
+        bringUpdate();
+    })
+
+    playGameBtn.on("click", () => {
+        console.log("game play"); // TODO
+    })
 })
